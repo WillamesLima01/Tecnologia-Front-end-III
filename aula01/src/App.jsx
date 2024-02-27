e03480159f2e0c7f365d15ea2f719beb042dcfac
WillamesLima01 <2022211510087@iesp.edu.br>
2024-02-27 19:23:05 -0300

    aula01

--- aula01/src/App.jsx
+++ aula01/src/App.jsx
@@ -9,19 +9,24 @@ function App() {
   return (
     <>
       <div>
-        <a href="budog.JPEG" target="_blank">   </a>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
       </div>
-      <h1>Meu Primeiro Projeto</h1>
+      <h1>Hello world</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          Conte mais {count}
+          Click aqui para enumeração {count}
         </button>
         <p>
-          Willames Lima {2+2}
+          Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Willames Lima
       </p>
     </>
   )
